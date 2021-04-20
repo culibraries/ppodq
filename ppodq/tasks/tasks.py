2ff137fa8396efc304262b48f74a5797652c3086
Mark Stacy <mbstacy@gmail.com>
2021-04-20 14:05:33 -0600

    initial commit ppod queue

---
+++ ppodq/tasks/tasks.py
@@ -0,0 +1,25 @@
+from celery.task import task
+import os
+import requests
+
+
+#Example task
+@task()
+def add(x, y):
+    """ Example task that adds two numbers or strings
+        args: x and y
+        return addition or concatination of strings
+    """
+    result = x + y
+    return result
+
+
+@tasks()
+def getDeliveryInfo(isbn):
+    """ Example task
+        return book info 
+    """
+    oasis_url= os.getenv('OASIS_URL',None)
+    # req= request.get(oasis_url)
+    # data=req.json()
+    return isbn
